4da934aaa17f81804aa58052d5b92cecf3bfecfa
E. OLLIVIER <hellojito@gmail.com>
2018-07-23 19:07:31 +0200

    method getInventoryPrice() completed

--- src/main/java/com/lambazon/domain/Product.java
+++ src/main/java/com/lambazon/domain/Product.java
@@ -6,6 +6,7 @@ public class Product {
 	private String name, description, details;
 	private int quantity;
 	private double price;
+	private double InventoryPrice; //created for getInventoryPrice()
 
 	public Product(int id, int quantity, double price, String name, String description) {
 		setId(id);
@@ -65,6 +66,11 @@ public class Product {
 
 	public double getInventoryPrice() {
 		// TODO Auto-generated method stub
-		return 0.0;
+		//price*quantity for total price of this product inventory
+		double price = getPrice();
+		int quantity = getQuantity();
+		double qu = (double)quantity;
+		InventoryPrice = price*qu;
+		return InventoryPrice;
 	}
 }
